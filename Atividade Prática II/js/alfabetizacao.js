f7c4c6699b6b39c0865fa583adae0c47acb3cf07
RenanCn <renan.rcn@hotmail.com>
2017-12-16 00:10:57 -0200

    didatica html/css/jquery

---
+++ Atividade Prática II/js/alfabetizacao.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  var ischecked_1 = $("input[name=radio1]:checked").val()
+  var ischecked_2 = $("input[name=radio2]:checked").val()
+  var ischecked_3 = $("input[name=radio3]:checked").val()
+  var ischecked_4 = $("input[name=radio4]:checked").val()
+
+  alert(ischecked);
+
+});
